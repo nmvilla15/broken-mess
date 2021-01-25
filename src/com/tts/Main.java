99e269ac5e81a489b0d437b3ff3507543be3195c
nmvilla15 <niki.villalpando@gmail.com>
2021-01-24 21:08:05 -0600

    first commit

---
+++ src/com/tts/Main.java
@@ -0,0 +1,29 @@
+package com.tts;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Hello World");
+        System.out.println("What is your first name?");
+        String firstName = userInput.nextLine();
+        System.out.printf("Hello %s %s!\n", firstName);
+
+
+        //error because data type does not match value
+        int greeting = "Hello World";
+        //error because the s is not capitalized, which is really annoying
+        string Title = "Help me"
+
+        String blah = "blah blah blah";
+        //I don't even know what this number is
+        int salaryexpectation = 10000000000000000000;
+        System.out.println(blah);
+
+    }
+
+}
+
+
+
+
